33c28c6ab63997bc95469a337360062bbc75d8a3
Ivan Lapunka <ivan_lapunka@gmail.com>
2021-02-09 17:07:57 +0300

    added @slf4j logging exclude common processing sql part from methods.

--- repository/src/main/java/all/repos/StudentRepositoryPostgres.java
+++ repository/src/main/java/all/repos/StudentRepositoryPostgres.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-//@Slf4j
+@Slf4j
 public class StudentRepositoryPostgres implements Repository<Student> {
 
     private final Datasource datasource = Datasource.getInstance();
@@ -43,59 +43,63 @@ public class StudentRepositoryPostgres implements Repository<Student> {
 
     @Override
     public Set<Student> findAll() {
-        Map<Integer, Student> mapStudent = new HashMap<>();
-        Map<Integer, Group> mapGroup = new HashMap<>();
-        Map<Integer, Teacher> mapTeacher = new HashMap<>();
+        Set<Student> result = new HashSet<>();
         try (Connection conn = datasource.getConnection();
              PreparedStatement stat = conn.prepareStatement(SQL_GET_ALL_STUDENT_COLUMNS);
              ResultSet res = stat.executeQuery()) {
-            while (res.next()) {
-                Integer teacherId = getRsInteger(res, SqlColumns.TEACHER_ID);
-                Integer studentId = getRsInteger(res, SqlColumns.STUDENT_ID);
-                Integer groupId = getRsInteger(res, SqlColumns.GROUP_ID);
-
-
-                mapStudent.putIfAbsent(studentId, new Student()
-                        .withAge(res.getInt(SqlColumns.STUDENT_AGE))
-                        .withLogin(res.getString(SqlColumns.STUDENT_LOGIN))
-                        .withPassword(res.getString(SqlColumns.STUDENT_PASSWORD))
-                        .withFirst_name(res.getString(SqlColumns.STUDENT_FIRST_NAME))
-                        .withLast_name(res.getString(SqlColumns.STUDENT_LAST_NAME))
-                        .withId(res.getInt(SqlColumns.STUDENT_ID))
-                        .withGroup(putIfAbsentAndReturn(mapGroup, groupId, new Group()
-                                    .withName(res.getString(SqlColumns.GROUP_NAME))
-                                    .withId(getRsInteger(res, SqlColumns.GROUP_ID))
-                                    .withTeacher(putIfAbsentAndReturn(mapTeacher, teacherId, new Teacher()
-                                                .withAge(res.getInt(SqlColumns.TEACHER_AGE))
-                                                .withLogin(res.getString(SqlColumns.TEACHER_LOGIN))
-                                                .withPassword(res.getString(SqlColumns.TEACHER_PASSWORD))
-                                                .withFirst_name(res.getString(SqlColumns.TEACHER_FIRST_NAME))
-                                                .withLast_name(res.getString(SqlColumns.TEACHER_LAST_NAME))
-                                                .withId(getRsInteger(res, SqlColumns.TEACHER_ID)))))));
-
-                mapStudent.computeIfPresent(studentId, (id, student) ->
-                        student
-                                .withGroup(mapGroup.get(groupId)));
-
-                mapTeacher.computeIfPresent(teacherId, (id, teacher) ->
-                        teacher
-                                .withGroup(mapGroup.get(groupId)));
-
-                mapGroup.computeIfPresent(groupId, (id, group) ->
-                        group
-                                .withStudent(mapStudent.get(studentId)));
-
-            }
-
+             result = rsToStudents(res);
         } catch (SQLException e) {
-            //log.error("sql error, during reading students");
+            log.error("sql error, during reading students");
             throw new DatabaseException(e);
         } catch (ClassNotFoundException e) {
-            //log.error("student ");
+            log.error("student ");
             e.printStackTrace();
         }
-        Set<Student> result = new HashSet<>(mapStudent.values());
         return result;
+    }
+
+    private Set<Student> rsToStudents(ResultSet res) throws SQLException {
+        Map<Integer, Student> mapStudent = new HashMap<>();
+        Map<Integer, Group> mapGroup = new HashMap<>();
+        Map<Integer, Teacher> mapTeacher = new HashMap<>();
+        while (res.next()) {
+            Integer teacherId = getRsInteger(res, SqlColumns.TEACHER_ID);
+            Integer studentId = getRsInteger(res, SqlColumns.STUDENT_ID);
+            Integer groupId = getRsInteger(res, SqlColumns.GROUP_ID);
+
+
+            mapStudent.putIfAbsent(studentId, new Student()
+                    .withAge(res.getInt(SqlColumns.STUDENT_AGE))
+                    .withLogin(res.getString(SqlColumns.STUDENT_LOGIN))
+                    .withPassword(res.getString(SqlColumns.STUDENT_PASSWORD))
+                    .withFirst_name(res.getString(SqlColumns.STUDENT_FIRST_NAME))
+                    .withLast_name(res.getString(SqlColumns.STUDENT_LAST_NAME))
+                    .withId(res.getInt(SqlColumns.STUDENT_ID))
+                    .withGroup(putIfAbsentAndReturn(mapGroup, groupId, new Group()
+                                .withName(res.getString(SqlColumns.GROUP_NAME))
+                                .withId(getRsInteger(res, SqlColumns.GROUP_ID))
+                                .withTeacher(putIfAbsentAndReturn(mapTeacher, teacherId, new Teacher()
+                                            .withAge(res.getInt(SqlColumns.TEACHER_AGE))
+                                            .withLogin(res.getString(SqlColumns.TEACHER_LOGIN))
+                                            .withPassword(res.getString(SqlColumns.TEACHER_PASSWORD))
+                                            .withFirst_name(res.getString(SqlColumns.TEACHER_FIRST_NAME))
+                                            .withLast_name(res.getString(SqlColumns.TEACHER_LAST_NAME))
+                                            .withId(getRsInteger(res, SqlColumns.TEACHER_ID)))))));
+
+            mapStudent.computeIfPresent(studentId, (id, student) ->
+                    student
+                            .withGroup(mapGroup.get(groupId)));
+
+            mapTeacher.computeIfPresent(teacherId, (id, teacher) ->
+                    teacher
+                            .withGroup(mapGroup.get(groupId)));
+
+            mapGroup.computeIfPresent(groupId, (id, group) ->
+                    group
+                            .withStudent(mapStudent.get(studentId)));
+
+        }
+        return new HashSet<>(mapStudent.values());
     }
 
     private static Integer getRsInteger(ResultSet rs, String colName) throws SQLException {
@@ -104,28 +108,19 @@ public class StudentRepositoryPostgres implements Repository<Student> {
 
     @Override
     public Optional<Student> find(Integer id) {
-        Student student = null;
+        Set<Student> result = new HashSet<>();
         try (Connection conn = datasource.getConnection();
-             PreparedStatement pStat = conn.prepareStatement(SQL_GET_STUDENT_BY_ID);
-        ) {
-            pStat.setInt(1, id);
-            ResultSet rs = pStat.executeQuery();
-            if (rs.next()) {
-                rs.getRow();
-                student = new Student()
-                        .withAge(rs.getObject(SqlColumns.STUDENT_AGE, Integer.class))
-                        .withLogin(rs.getString(SqlColumns.STUDENT_LOGIN))
-                        .withPassword(rs.getString(SqlColumns.STUDENT_PASSWORD))
-                        .withFirst_name(rs.getString(SqlColumns.STUDENT_FIRST_NAME))
-                        .withLast_name(rs.getString(SqlColumns.STUDENT_LAST_NAME))
-                        .withId(rs.getInt(SqlColumns.STUDENT_ID));
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+             PreparedStatement stat = conn.prepareStatement(SQL_GET_STUDENT_BY_ID);
+             ResultSet res = stat.executeQuery()) {
+             result = rsToStudents(res);
+        } catch (SQLException e) {
+            log.error("sql error, during reading students");
+            throw new DatabaseException(e);
         } catch (ClassNotFoundException e) {
+            log.error("driver class not found");
             e.printStackTrace();
         }
-        return Optional.ofNullable(student);
+        return result.stream().findAny();
     }
 
     private static <V, K> V putIfAbsentAndReturn (Map<K, V> map, K key, V value) {
